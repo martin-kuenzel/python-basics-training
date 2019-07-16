8877ad55d17bc7f9281dd2c04c5340d4d80a19c3
mlkone <martin.kuenzel@hs-bochum.de>
2019-07-16 17:37:01 +0200

    using a context manager (keyword "with"), writing binary copies

--- python_basics_fileioops.py
+++ python_basics_fileioops.py
@@ -2,7 +2,8 @@
 import os
 
 # create a testfiles_path in the scripts directory [if not existant]
-testfiles_path = os.path.join( os.path.realpath( os.path.dirname(__file__) ), 'testfiles' )
+script_dir = os.path.realpath( os.path.dirname(__file__) )
+testfiles_path = os.path.join( script_dir, 'testfiles' )
 if not os.path.exists( testfiles_path ):
     os.mkdir( testfiles_path )
 
@@ -17,14 +18,14 @@ opened_file.close()
 opened_file = open(testfile,mode='a', encoding='utf-8')
 print( f'''
     Appending more text to the file object of type {type(opened_file)}
-    which resides @{os.path.realpath(os.path.dirname(testfile))}
-    and was created by {os.path.realpath(__file__)}.
+    which resides @{testfiles_path}
+    and was created by {script_dir}.
 ''', file=opened_file)
 
 print( f'''
     And since it's still opened, we can just add more lines at this time.
 
-    And this is what {os.path.realpath(__file__)} looks like:
+    And this is what {script_dir} looks like:
 
 ''', file=opened_file)
 
@@ -36,8 +37,55 @@ for line in the_whole_script:
 the_whole_script.close()
 opened_file.close()
 
-### read the contents of the testfile and print each line to std.out
-opened_file = open(testfile,mode='r',encoding='utf-8')
-for line in opened_file:
-    print(line,end='')
-opened_file.close()
+### USING A CONTEXT MANAGER (auto closes file)
+with open(testfile,mode='r',encoding='utf-8') as opened_file:
+    ### reads only one line from the file, prints that one out and goes to the next line
+    #print( opened_file.readline(), end='' )
+    
+    ### read the contents of the testfile and print each line to std.out (possible ways)
+    
+    #### bad for large files
+    #print(  opened_file.readlines() )
+    
+    #### efficient way to read all the contents of a file
+    #for line in opened_file:
+    #    print(line,end='')
+
+    #### reads n characters of a file until no more chars available
+    #size_to_read = 100
+    #f_contents = opened_file.read(size_to_read)
+    #opened_file.tell() ## outputs cursor position
+    #opened_file.seek() ## can be used to change the position of the cursor
+    #while len(f_contents)>0:
+    #    print(f_contents,end='')
+    #    f_contents = opened_file.read(size_to_read)
+    pass
+
+with open(testfile,mode='a',encoding='utf-8') as f:
+    print('''
+        this is the better method to write/append content to a file
+    ''', file=f)
+    ### write the contents of this script into the testfile
+    with open(__file__,mode='r',encoding='utf-8') as the_whole_script:
+        for line in the_whole_script:
+            print(line,file=f,end='')
+
+with open(testfile,mode='r',encoding='utf-8') as f:
+    for line in f:
+        print(line,end='')
+
+### copy binary files 
+image_file = os.path.join(script_dir,'face-wink.png')
+image_file_copy = os.path.join(testfiles_path,'face-wink_copy.png')
+with open(image_file,'rb') as img:
+    with open(image_file_copy,'wb') as img_copy:
+    #    for l in img:
+    #        img_copy.write(l)
+    #### alternatively
+        chunk_size = 2048
+        chunk = img.read(chunk_size)
+        while len(chunk) > 0:
+            img_copy.write(chunk)
+            chunk = img.read(chunk_size)
+
+
