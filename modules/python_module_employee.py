49c7595089ddbc675cb5afca38eb8e09802567c4
mlkone <martin.kuenzel@hs-bochum.de>
2019-07-20 10:23:56 +0200

    basics of the classes system second part 5: example employee classes with additional overloading of operators

--- modules/python_module_employee.py
+++ modules/python_module_employee.py
@@ -8,6 +8,10 @@ class Employee:
     salary_raise_rate = 1.02
     paid = 0
 
+    @staticmethod
+    def is_workday(day):
+        return -1 < day.weekday() < 5
+
     @classmethod
     def set_raise_amount(cls,amount):
         cls.salary_raise_rate = amount
@@ -16,10 +20,6 @@ class Employee:
     def from_string(cls,string):
         firstname,name = string.split('-')
         return cls(firstname,name)
-
-    @staticmethod
-    def is_workday(day):
-        return -1 < day.weekday() < 5
 
     def __init__(self, firstname = 'John', name = 'Doe', salary = 1000):
         self.emp_no = Employee.emp_no
@@ -100,17 +100,6 @@ class Employee:
 # ''')
 
 # print( f'Today {date.today().strftime("%c")} is{ [" not "," "][int( Employee.is_workday( date.today() ) )] }a workday')
-
-## TESTBASE CLASS INHERITANCE ##
-
-class Developer(Employee):
-    salary_raise_rate = 1.05
-    def __init__(self,firstname,name,prog_lang,salary=2000):
-        self.prog_lang = prog_lang
-        super().__init__(firstname,name,salary)
-    
-    def __str__(self):
-        return f'{super().__str__()}, Programming language: {self.prog_lang}'
     
 class Manager(Employee):
     salary_raise_rate = 1.075
@@ -122,22 +111,30 @@ class Manager(Employee):
         super().__init__(firstname,name,salary)
     
     def add_emp(self, emp):
-        if emp not in self.employees:
+        if not self is emp and emp not in self.employees:
             self.employees.append(emp)
             print(f'{emp.fullname} is now managed by {self.fullname}')
     
     def remove_emp(self, emp):
-        if emp in self.employees:
+        if not self is emp and emp in self.employees:
             self.employees.remove(emp)
             print(f'{emp.fullname} is no longer managed by {self.fullname}')
     
     def __add__(self, emp):
-        if not self is emp:
-            self.add_emp(emp)
+        self += emp
+        return self
 
     def __sub__(self, emp):
-        if not self is emp:
-            self.remove_emp(emp)
+        self -= emp
+        return self
+
+    def __iadd__(self, emp):
+        self.add_emp(emp)
+        return self
+
+    def __isub__(self, emp):
+        self.remove_emp(emp)
+        return self
     
     def emps_str(self):
         str = f'-- Employees managed by {self.fullname} --'
@@ -152,7 +149,43 @@ class Manager(Employee):
 
     def __str__(self):
         return f'{super().__str__()}\n{self.emps_str()}'
+
+class Developer(Employee):
+    salary_raise_rate = 1.05
+    prog_langs = []
     
+    def __init__(self,firstname,name,prog_langs = None,salary=2000):
+        if prog_langs is None: 
+            raise TypeError(f'{__class__.__name__} need at least one prog_lang!')
+        elif type(prog_langs) is type(str()): 
+            prog_langs = [prog_langs]
+        elif not type(prog_langs) is type([]):
+            raise TypeError()
+
+        for prog_lang in prog_langs:
+            if not prog_lang in self.prog_langs:
+                self.prog_langs.append( prog_lang )
+
+        super().__init__(firstname,name,salary)
+    
+    def __iadd__(self, prog_langs):
+        if type(prog_langs) is type(str()): 
+            prog_langs = [prog_langs]
+        elif not type(prog_langs) is type([]):
+            raise TypeError()
+
+        for prog_lang in prog_langs:
+            if not prog_lang in self.prog_langs:
+                print(f'{self.fullname} learned {prog_lang}')
+                self.prog_langs.append( prog_lang )
+
+        return self
+
+    
+    def __str__(self):
+        return f'{super().__str__()}, Programming language: {self.prog_langs}'
+
+## TESTBASE CLASS INHERITANCE, OPERATOR OVERLOADING ##
 
 dev_0 = Developer('Ackthay', 'Neet','JavaScript')
 dev_0.fullname = 'Hacka|Lot'
@@ -161,18 +194,21 @@ del dev_0.fullname
 dev_0.fullname = 'Ackthay|Loot'
 
 dev_0.pay()
-dev_0.raise_salary()
-
-dev_0.pay()
 
 man_0 = Manager('Owen','Ju')
 man_0.raise_salary()
 man_0.pay()
 
-#man_0.add_emp(dev_0)
-man_0 + dev_0
-man_0.print_emps()
+man_0 -= dev_0
+man_0 += dev_0
+man_0 += dev_0
 
-#man_0.remove_emp(dev_0)
-man_0 - dev_0
-man_0.print_emps()
+dev_0 += ['Python','JavaScript','C++']
+dev_0.raise_salary()
+dev_0.pay()
+
+dev_0 += 'R-Programming Lang'
+dev_0.raise_salary()
+dev_0.pay()
+
+man_0 -= dev_0
